942174ec4e30e9f6c5110cbbb33ec7410eff7d25
Anne Marie Charmaine Mendoza <anne.marie.c.mendoza@accenture.com>
2026-05-11 01:05:29 +0000

    completed changes for Day 4 exercise

--- webapp/controller/JSONBinding.controller.js
+++ webapp/controller/JSONBinding.controller.js
@@ -1,10 +1,64 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
+    "sap/ui/core/mvc/Controller",
+    "../model/formatter"
+], (Controller, formatter) => {
     "use strict";
 
     return Controller.extend("sapips.training.jsonbinding.controller.JSONBinding", {
-        onInit() {
+        
+       formatter: formatter,
+
+        onInit: function () {
+
+            // ---------------------------Bind address---------------------------- //
+
+            // use onInit for pre-page loading of below values
+            var addressData = {
+                inpEid: "anne.marie.c.mendoza",
+                cbxEnabled: true,
+                Address: {
+                    Street: "15 Font St",
+                    City: "Metro Manila",
+                    Zip: "1700",
+                    Country: "Philippines"
+                },
+                SalesAmount: 15000,
+                CurrencyCode: "PHP"
+            };
+
+            //set model binding for addressData
+            var addressModel = new sap.ui.model.json.JSONModel(addressData);
+
+            //set addressData to bind to view and name it as "address" because models can only handle one unnamed
+            this.getView().setModel(addressModel, "address");
+
+            // -----------------------Bind Products-----------------------------//
+            //set model binding for Products.json               
+            var productsModel = new sap.ui.model.json.JSONModel();
+
+            productsModel.attachRequestCompleted(function () {
+                console.log("Products loaded:", productsModel.getData());
+            });
+
+            productsModel.loadData("/model/Products.json");
+
+            //set addressData to bind to view and name it as "products" because models can only handle one unnamed   
+            this.getView().setModel(productsModel, "products");
+
+
+        },
+
+        //add row select event
+        onRowSelect: function (listEvent) {
+
+            var listSelectedItem = listEvent.getParameter("listItem");   // clicked row; listItem is predefined
+            var listContext = listSelectedItem.getBindingContext("products"); // get data from products model
+
+            this.getView().setBindingContext(listContext, "products"); // set context for form
+
         }
+
+
     });
+
 });
